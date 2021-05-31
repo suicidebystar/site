760b28174033e16500c8ce87f4b505aaf7f1e65a
César Suárez <suarez.ortega.cesar@gmail.com>
2021-05-31 20:25:53 +0200

    check bootstrap installation with an improved layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,42 @@ import "./header.scss";
 
 export const Header = () => (
   <>
-    <h1>SuicideByStar</h1>
-    <ul>
-      <li>
-        <Link to="/">🏠</Link>
-      </li>
-      <li>
-        <Link to="/posts">Posts</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
-    <hr />
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          SuicideByStar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-link active" aria-current="page">
+                🏠
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/posts" className="nav-link">
+                Posts
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </>
 );
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Header } from "../Header/Header";
 
 export const Layout = ({ children }) => (
-  <main>
+  <>
     <Header />
-    <article>{children}</article>
-    <section>footer</section>
-  </main>
+    <main className="container">
+      <article>{children}</article>
+      <section>footer</section>
+    </main>
+  </>
 );
