006ae907cde1c53a016da512a65d48c1b5bf991e
César Suárez <suarez.ortega.cesar@gmail.com>
2022-07-29 12:13:58 +0200

    show hamburguer menu in mobile view

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,40 +2,40 @@ import { Link } from "gatsby";
 import React from "react";
 import "./header.scss";
 import { StaticImage } from "gatsby-plugin-image";
+import { slide as Menu } from "react-burger-menu";
 
-export const Header = () => (
-  <>
-    {/* <nav className="header">
-      <Link to="/">
-        <h1 className="logo">
-          Suicide<span className="logo__accent">By</span>Star
-        </h1>
-      </Link>
-      <ul className="header__menu">
-        <li>
-          <Link to="/posts" className="header__link">
-            Posts
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className="header__link">
-            About
-          </Link>
-        </li>
-      </ul>
-    </nav> */}
-    <nav className="header header--mobile">
-      <Link to="/">
-        <h1 className="logo logo--mobile">
-          <StaticImage
-            src="../../images/logo.png"
-            alt="logo"
-            className="logo__image"
-            imgStyle={{ objectFit: "contain" }}
-          />
-          Suicide<span className="logo__accent">By</span>Star
-        </h1>
-      </Link>
-    </nav>
-  </>
-);
+export const Header = () => {
+  return (
+    <>
+      <nav className="header">
+        <Link to="/">
+          <h1 className="logo">
+            <StaticImage
+              src="../../images/logo.png"
+              alt="logo"
+              className="logo__image"
+              imgStyle={{ objectFit: "contain" }}
+            />
+            Suicide<span className="logo__accent">By</span>Star
+          </h1>
+        </Link>
+        <div className="header__menu-mobile">
+          <Menu right width={"100%"}>
+            <Link className="menu-item" href="/">
+              Inicio
+            </Link>
+            <Link className="menu-item" href="/about">
+              Programas
+            </Link>
+            <Link className="menu-item" href="/contact">
+              Monográficos
+            </Link>
+            <Link className="menu-item" href="/contact">
+              Archivo
+            </Link>
+          </Menu>
+        </div>
+      </nav>
+    </>
+  );
+};
