0678a961c80deba300dd6548a4816af44c80a808
César Suárez <suarez.ortega.cesar@gmail.com>
2022-11-17 19:46:25 +0100

    fix feed paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,6 +92,7 @@ module.exports = {
                     frontmatter {
                       title
                       date
+                      path
                     }
                     html
                   }
