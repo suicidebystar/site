1dec346d0d9ba2b8219ccb4813df94bc8873cdf4
César Suárez <suarez.ortega.cesar@gmail.com>
2022-12-19 11:13:24 +0100

    add international top

--- src/components/AlbumList/AlbumItem.jsx
+++ src/components/AlbumList/AlbumItem.jsx
@@ -6,7 +6,6 @@ export function AlbumItem({ title, children, position, ...props }) {
     <section className="album-item" {...props}>
       <div className="album-item__position">{position}</div>
       <div className="album-item__cover">{children}</div>
-
       <div className="album-item__title">{title}</div>
     </section>
   );
